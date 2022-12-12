6b11ccbac1390e631f0df84cddc0237ada6badaa
Dottellini <kevinklauser@aol.de>
2022-12-12 12:58:20 +0100

    updated 9Praktikum

--- 9Praktikum/toilet.java
+++ 9Praktikum/toilet.java
@@ -1,29 +1,32 @@
-import java.util.ListIterator;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-//Not finished
-
 class Roll {
-    boolean emptied = false;
-
-    boolean isEmpty() {
-        return emptied;
-    }
-
-    void use() {
-        emptied = true;
-    }
+    Roll next;
+    Roll previous;
 }
 
-class Spender {
-    ConcurrentLinkedQueue<Roll> queue = new ConcurrentLinkedQueue<>();
+class RQueue {
+    Roll head, tail;
 
-    boolean add(Roll roll) {
-        queue.add(roll);
-        return true;
+    void add(Roll r) {
+        if(tail == null && head == null) {
+            head = r;
+        } else {
+            r.next = tail;
+            tail.previous = r;
+        }
+
+        tail = r;
     }
 
     Roll remove() {
-        return queue.remove();
+        Roll removedElement = head;
+        if(head == tail) {
+            head = null;
+            tail = null;
+            return removedElement;
+        };
+        head = head.previous;
+        head.next = null;
+
+        return removedElement;
     }
 }
